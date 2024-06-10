4ef4b429969a31c7ae4268525648d7fd394e1eb9
musty4real2 <musty4real2@gmail.com>
2024-06-10 17:19:42 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import {Box} from '@mui/material';
+import './App.css';
 
 const App = () => {
 return (
-    <div>App</div>
+    <Box>App</Box>
 )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
 
-const root =ReactDOM.createRoot(Document.getElementById("root"));
+const root =ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <App  />
