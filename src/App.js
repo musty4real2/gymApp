e79775db94606fbb6c6ea2a69c0366f55607c163
musty4real2 <musty4real2@gmail.com>
2024-06-10 17:55:15 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import {Box} from '@mui/material';
 import './App.css';
-
+import Home from './pages/Home';
+import Exercisedetail from './pages/Exercisedetail';
+import Navbar from './components/Navbar';
 const App = () => {
 return (
-    <Box>App</Box>
+    <Box width='400px'>
+        <NavBar />
+        <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='/exercise/:id/' element={ <ExerciseDetail /> } />
+        </Routes>
+    </Box>
 )
 }
 
