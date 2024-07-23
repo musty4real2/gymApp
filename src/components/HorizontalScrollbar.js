7b48b97b9d055f8b123b32f1d661593e78e4827f
musty4real2 <musty4real2@gmail.com>
2024-07-23 21:26:22 +0100

    listing exercises

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import BodyPart from './BodyPart';
-import {ScrollMenu, VisibilityContext} from 'react-horizontal-scrolling-menu';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import RightArrowIcon from '../assets/icons/right-arrow.png'
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import { useContext } from 'react';
 
+const LeftArrow = () => {
+    const { scrollPrev } = useContext(VisibilityContext);
 
+    return (
+        <Typography onClick={() => scrollPrev()} className="right-arrow">
+            <img src={LeftArrowIcon} alt="right-arrow" />
+        </Typography>
+    );
+};
+
+const RightArrow = () => {
+    const { scrollNext } = useContext(VisibilityContext);
+
+    return (
+        <Typography onClick={() => scrollNext()} className="left-arrow">
+            <img src={RightArrowIcon} alt="right-arrow" />
+        </Typography>
+    );
+};
 function HorizontalScrollbar({ data, bodyPart, setBodyPart }) {
     return (
-        <ScrollMenu>
+        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
             {data.map((item) => (
                 <Box
                     key={item.id || item}
@@ -14,7 +35,7 @@ function HorizontalScrollbar({ data, bodyPart, setBodyPart }) {
                     title={item.id || item}
                     m="0 40px"
                 >
-                <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+                    <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
 
                 </Box>
             )
